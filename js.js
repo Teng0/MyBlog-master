1b29335e7f42bbceea2264b4982b05b211683a45
sdsd <dcs>
2018-02-03 17:16:52 +0400

    adding paralax

---
+++ js.js
@@ -0,0 +1,14 @@
+
+
+
+
+/*$(document).ready(function(){
+
+    const lis2=document.getElementsByTagName("li");
+
+    lis2[2].addEventListener("click",function () {
+        console.log("Hello!");
+    })
+});*/
+
+/*$('.parallax-window').parallax({imageSrc: 'img/global-education.jpg'});*/
